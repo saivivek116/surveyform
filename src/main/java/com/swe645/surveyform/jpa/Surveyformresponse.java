af2971d03645ba973e2ef73d3f71f4d9bd5a0dcb
saivivek mac <saivivek116@gmail.com>
2024-03-31 14:35:53 -0400

    modified db schema

--- src/main/java/com/swe645/surveyform/jpa/Surveyformresponse.java
+++ src/main/java/com/swe645/surveyform/jpa/Surveyformresponse.java
@@ -27,6 +27,28 @@ public class Surveyformresponse {
 
     private Date date_of_survey;
 
+    @Enumerated(EnumType.STRING)
+    private CampusPreference likedMostAboutCampus;
+
+    @Enumerated(EnumType.STRING)
+    private InterestSource interestSource;
+
+    @Enumerated(EnumType.STRING)
+    private RecommendationLikelihood recommendationLikelihood;
+
+    public enum CampusPreference {
+        STUDENTS, LOCATION, CAMPUS, ATMOSPHERE, DORM_ROOMS, SPORTS
+    }
+
+    public enum InterestSource {
+        FRIENDS, TELEVISION, INTERNET, OTHER
+    }
+
+    public enum RecommendationLikelihood {
+        VERY_LIKELY, LIKELY, UNLIKELY
+    }
+
+
     @PrePersist
     public void prePersist() {
         if (date_of_survey == null) { // Only set the current date if it hasn't been set already
